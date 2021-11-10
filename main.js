30b0834a905119efe526eb9b3a64fbdec84e6ac4
MontyWilliams <3650@holbertonschool.com>
2021-11-10 17:07:27 -0600

    got 1nch working!

--- main.js
+++ main.js
@@ -15,8 +15,19 @@ async function listAvailableTokens() {
   const result = await Moralis.Plugins.oneInch.getSupportedTokens({
     chain: "eth", // The blockchain you want to use (eth/bsc/polygon)
   });
-  console.log(result.tokens);
   tokens = result.tokens;
+  let parent = document.getElementById("token_list");
+  for (const address in tokens){
+    let token = tokens[address];
+    let div = document.createElement("div");
+  div.className = "token_row";
+  let html = `
+  <img class="token_list_img" src="${token.logoURI}">
+        <span class="token_list_text">${token.symbol}</span>
+        `;
+        div.innerHTML = html;
+        parent.appendChild(div);
+  }
 }
 
 async function login() {
@@ -43,6 +54,8 @@ function openModal(){
 function closeModal(){
   document.getElementById("token_modal").style.display = "none";
 }
+
+init();
 
 document.getElementById("modal_close").onclick = closeModal;
 document.getElementById("from_token_select").onclick = openModal;
