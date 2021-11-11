4343bb3bef93b4ee1f23d4302500fef4ac84b05f
MontyWilliams <3650@holbertonschool.com>
2021-11-11 08:51:29 -0600

    getting type error when trying to get amount

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const serverUrl = "https://blrjwk58waec.usemoralis.com:2053/server";
 const appId = "EWWUBg2MHRZEtrK8pWA9VRlcaobAAVglh8vKlTnJ";
 Moralis.start({ serverUrl, appId });
 
+let currentTrade = {};
+let currentSelectSide;
+let tokens;
+
 async function init() {
   await Moralis.start({ serverUrl, appId });
   await Moralis.enableWeb3();
@@ -11,22 +15,47 @@ async function init() {
     document.getElementById("swap_button").disabled = false;
   }
 }
+
 async function listAvailableTokens() {
   const result = await Moralis.Plugins.oneInch.getSupportedTokens({
     chain: "eth", // The blockchain you want to use (eth/bsc/polygon)
   });
   tokens = result.tokens;
   let parent = document.getElementById("token_list");
-  for (const address in tokens){
+  for (const address in tokens) {
     let token = tokens[address];
     let div = document.createElement("div");
-  div.className = "token_row";
-  let html = `
-  <img class="token_list_img" src="${token.logoURI}">
+    div.setAttribute("data-address", address);
+    div.className = "token_row";
+    let html = `
+        <img class="token_list_img" src="${token.logoURI}">
         <span class="token_list_text">${token.symbol}</span>
         `;
-        div.innerHTML = html;
-        parent.appendChild(div);
+    div.innerHTML = html;
+    div.onclick = () => {
+      selectToken(address);
+    };
+    parent.appendChild(div);
+  }
+}
+
+function selectToken(address) {
+  closeModal();
+  console.log(tokens);
+  currentTrade[currentSelectSide] = tokens[address];
+  console.log(currentTrade);
+  renderInterface();
+  getQuote();
+}
+
+function renderInterface(){
+  if(currentTrade.from){
+    document.getElementById("from_token_img").src = currentTrade.from.logoURI;
+    document.getElementById("from_token_text").innerHTML = currentTrade.from.symbol;
+  }
+  if(currentTrade.to){
+  document.getElementById("to_token_img").src = currentTrade.to.logoURI;
+  document.getElementById("to_token_text").innerHTML = currentTrade.to.symbol;
   }
 }
 
@@ -48,17 +77,40 @@ async function logOut() {
   console.log("logged out");
 }
 
-function openModal(){
+function openModal(side){
+  currentSelectSide = side;
   document.getElementById("token_modal").style.display = "block";
 }
 function closeModal(){
   document.getElementById("token_modal").style.display = "none";
 }
 
+async function getQuote() {
+  if (!currentTrade.from || !currentTrade.to || !document.getElementById("from_amount").value) return;
+
+  let amount = Number(document.getElementById("from_amount").value * 10 ** currentTrade.from.decimals);
+
+  const quote = await Moralis.Plugins.oneInch.quote({
+    chain: "eth", // The blockchain you want to use (eth/bsc/polygon)
+    fromTokenAddress: currentTrade.from.address, // The token you want to swap
+    toTokenAddress: currentTrade.to.address, // The token you want to receive
+    amount: amount,
+  })
+  console.log(quote);
+  // document.getElementById("gas_estimate").innerHTML = quote.estimatedGas;
+  document.getElementById("to_amount").value = quote.toTokenAmount / 10 ** quote.toToken.decimals;
+}
+
 init();
-
+ 
 document.getElementById("modal_close").onclick = closeModal;
-document.getElementById("from_token_select").onclick = openModal;
-
+document.getElementById("from_token_select").onclick = () => {
+  openModal("from");
+};
+document.getElementById("to_token_select").onclick = () => {
+  openModal("to");
+};
 document.getElementById("login_button").onclick = login;
+document.getElementById("from_amount").onblur = getQuote;
+
 // document.getElementById("btn-logout").onclick = logOut;
